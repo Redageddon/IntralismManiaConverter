1a3192e659e311a9214d8b9fc800e469dd02483a
Rubiksmaster02 <therealrubiksmaster@gmail.com>
2020-11-16 13:18:55 -0800

    changed output type to library

--- IntralismManiaConverter/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using IntralismManiaConverter;
-
-Converter.ConvertManiaToIntralism(
-    @"C:\Users\Rubiksmaster02\RiderProjects\IntralismManiaConverter\IntralismManiaConverter\bin\Debug\net5\ExampleOsu\a.osu",
-    @"C:\Users\Rubiksmaster02\RiderProjects\IntralismManiaConverter\IntralismManiaConverter\bin\Debug\net5\Output");
